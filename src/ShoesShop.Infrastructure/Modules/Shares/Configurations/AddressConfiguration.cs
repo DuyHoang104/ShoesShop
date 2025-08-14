f686e8d123ed0df9533ba29e761fb64116eaf8b1
Arrisd <hoangtd.21it@vku.udn.vn>
2025-08-14 18:38:43 +0700

    hoangtd/feature/configurations-for-migrations

---
+++ src/ShoesShop.Infrastructure/Modules/Shares/Configurations/AddressConfiguration.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShoesShop.Domain.Modules.Shares.Entities;
+
+namespace ShoesShop.Infrastructure.Modules.Shares.Configurations;
+
+public class AddressConfiguration : IEntityTypeConfiguration<Address>
+{
+    public void Configure(EntityTypeBuilder<Address> builder)
+    {
+        builder.ToTable("Addresses");
+        builder.Property(e => e.AddressLine1).HasField("_addressLine1").HasMaxLength(100).IsRequired();
+        builder.Property(e => e.City).HasField("_city").HasMaxLength(100);
+        builder.Property(e => e.Country).HasField("_country").HasMaxLength(100);
+
+        builder.HasMany(e => e.Orders).WithOne(e => e.Address).HasForeignKey(e => e.AddressId).OnDelete(DeleteBehavior.NoAction);
+        builder.HasOne(e => e.User).WithMany(e => e.Addresses).HasForeignKey(e => e.UserId).OnDelete(DeleteBehavior.NoAction);
+    }
+}
---
+++ src/ShoesShop.Infrastructure/Modules/Users/Configurations/UserConfiguration.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShoesShop.Domain.Modules.Users.Entities;
+
+namespace ShoesShop.Infrastructure.Modules.Users.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users");
+
+        builder.Property(e => e.UserName).HasField("_userName").HasMaxLength(100).IsRequired();
+        builder.Property(e => e.Password).HasField("_password").HasMaxLength(100).IsRequired();
+        builder.Property(e => e.DateOfBirth).HasField("_dateOfBirth").IsRequired();
+        builder.Property(e => e.Email).HasField("_email").HasMaxLength(100).IsRequired();
+        builder.Property(e => e.Phone).HasField("_phone").HasMaxLength(20);
+        builder.Property(e => e.Status).HasField("_status");
+        builder.Property(e => e.Gender).HasField("_gender");
+
+        builder.HasMany(e => e.Addresses).WithOne(e => e.User).HasForeignKey(e => e.UserId).OnDelete(DeleteBehavior.Cascade);
+        builder.HasMany(e => e.Orders).WithOne(e => e.User).HasForeignKey(e => e.UserId).OnDelete(DeleteBehavior.Cascade);
+        builder.HasMany(e => e.CartItems).WithOne(e => e.User).HasForeignKey(e => e.UserId).OnDelete(DeleteBehavior.Cascade);
+    }
+}
